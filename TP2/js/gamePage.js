85bc9d895e8a0d65cf8e73faf0cc5e6568bbf915
Krymer <etemagus@gmail.com>
2023-11-02 14:56:54 -0300

    canvas dimension change

--- TP2/js/gamePage.js
+++ TP2/js/gamePage.js
@@ -1,22 +1,28 @@
+const canvas = document.getElementById('canvasGame');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 1920;
+const CANVAS_HEIGHT = canvas.height = 800;
+import {Token} from'./fourInLine/Token.js';
+import {Table} from'./fourInLine/Table.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let board = new Table(4,canvas,ctx);
+    board.draw();
+})
 
 document.getElementById("canvasGame").addEventListener("click", (e)=>{
-    console.log(e);
-    const canvas = document.getElementById("canvasGame"); 
-    reac = canvas.getBoundingClientRect();
+    console.log(canvas.clientWidth);
+    let reac = canvas.getBoundingClientRect();
+    new Token('/TP2/images/4inline/alien.png', 30, null,e.clientX-reac.left, e.clientY-reac.top,ctx).draw();
     drawCircle(e.clientX-reac.left, e.clientY-reac.top);
 });
 
 
-
+console.log(ctx)
 
 
 function drawCircle(x,y){
-    const canvas = document.getElementById("canvasGame");
-    const ctx = canvas.getContext("2d");
-    let radius = 30;
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, 2 * Math.PI);
-    ctx.stroke();
-    console.log(x,y);
+
 }
     
