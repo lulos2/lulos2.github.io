7701a5416b3a159315f091a082dc99ff8f3166d2
Krymer <etemagus@gmail.com>
2023-11-03 15:14:18 -0300

    token and table attributes

--- TP2/js/fourInLine/Table.js
+++ TP2/js/fourInLine/Table.js
@@ -1,7 +1,25 @@
-class Table {
+export class Table {
+ 
+    #rowToWin;
+    #canvas;
+    #ctx;
+    #rows;
+    #columns;
+    #board;
 
+    constructor (rowsToWin,canvas,ctx){
+        this.#rowToWin = rowsToWin;
+        this.#canvas = canvas;
+        this.#ctx = ctx;
 
+    }
     
+    draw () {
+        let width = this.#canvas.clientWidth;
+        let height = this.#canvas.clientHeight;
+
+        this.#ctx.fillRect(0,0,width,height);
+    }
 
 
 }
--- TP2/js/fourInLine/Token.js
+++ TP2/js/fourInLine/Token.js
@@ -1,21 +1,45 @@
-class Token {
+export class Token {
     #image;
     #radio;
     #owner;
-    
+    #currentX;
+    #currentY;
+    #ctx;
 
-    constructor (image,radio,owner,context){
-        this.#image = image;
+    constructor (image,radio,owner,x,y,ctx){
+        this.#image = image ? new Image() : image;
         this.#radio = radio;
         this.#owner = owner;
-        this.context = context;
+        this.#currentX = x;
+        this.#currentY = y;
+        this.#ctx = ctx;
+        if (image) {
+            this.#image.src = image;
+            this.#image.width = this.#radio;
+            this.#image.height = this.#radio;
+        }
+        console.log(image, this.#image);
     }
 
-
-    create(posX,posY){
-
+    draw(){
+        let radius = 30;
+        this.#ctx.beginPath();
+        this.#ctx.arc(this.#currentX, this.#currentY, radius, 0, 2 * Math.PI);
+        this.#ctx.drawImage(this.#image, this.#currentX, this.#currentY, this.#radio, this.#radio);
+        console.log(this.#image);
     }
     
+    setPosition(x,y){
+        this.#currentX = x;
+        this.#currentY = y;
+    }
 
+    getY(){
+        return this.#currentY;
+    }
+
+    getX(){
+        return this.#currentX;
+    }
 
 }
